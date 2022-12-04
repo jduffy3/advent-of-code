f5a5b200bef42fba2d5327674efdc6d1bfb80cbd
James Duffy <jduffy3@gmail.com>
2022-12-04 09:14:56 -0500

    part two

--- 2022/day-4/solution.go
+++ 2022/day-4/solution.go
@@ -18,33 +18,40 @@ func main() {
 
   scanner := bufio.NewScanner(file)
   
-  fullyContainedCount := 0
+  containsCount, overlapCount := 0, 0
   for scanner.Scan() {
     sectionPair := strings.Split(scanner.Text(), ",")
     s1, s2 := strings.Split(sectionPair[0], "-"), strings.Split(sectionPair[1], "-")
 
-    if fullyContainsEither(s1, s2) {
-      fullyContainedCount += 1
+    if fullyContains(s1, s2) || fullyContains(s2, s1) {
+      containsCount += 1
+    }
+
+    if containsOverlap(s1, s2) {
+      overlapCount += 1
     }
   }
-  fmt.Println(fmt.Sprintf("%d assignments fully contains the other", fullyContainedCount))
+  fmt.Println(fmt.Sprintf("Contains count: %d, Overlap count :%d", containsCount, overlapCount))
 }
 
-func fullyContainsEither(s1, s2 []string) bool {
-  return (start(s1) <= start(s2) && end(s1) >= end(s2)) ||
-  (start(s2) <= start(s1) && end(s2) >= end(s1))
+func containsOverlap(s1, s2 []string) bool {
+  return start(s1) <= end(s2) && end(s1) >= start(s2)
+}
+
+func fullyContains(s1, s2 []string) bool {
+  return (start(s1) <= start(s2) && end(s1) >= end(s2))
 }
 
 func start(section []string) int {
-  i, err := strconv.Atoi(section[0])
-  if err != nil {
-    os.Exit(1)
-  }
-  return i
+  return parseInt(section[0])
 }
 
 func end(section []string) int {
-  i, err := strconv.Atoi(section[1])
+  return parseInt(section[1])
+}
+
+func parseInt(s string) int {
+  i, err := strconv.Atoi(s)
   if err != nil {
     os.Exit(1)
   }
